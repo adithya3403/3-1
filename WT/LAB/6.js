6fed1b59bc22100d04bfb86c1c2a6c7a7442d42d
adithya3403 <a.dty.3403@gmail.com>
2022-11-27 13:44:12 +0530

    Create 6.js

---
+++ WT/LAB/6.js
@@ -0,0 +1,10 @@
+// 6. Write a JavaScript function to check whether an object contains given property.
+
+let stu = {
+    name: "John",
+    age: 25,
+    college: "KMIT"
+};
+
+console.log(stu.hasOwnProperty("name"));
+console.log(stu.hasOwnProperty("id"));
