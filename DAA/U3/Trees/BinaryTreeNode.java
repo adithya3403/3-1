045cfdc60d9d43c30568eac4430df4c7049448a1
adithya3403 <a.dty.3403@gmail.com>
2022-11-28 19:11:37 +0530

    Create BinaryTreeNode.java

---
+++ DAA/U3/Trees/BinaryTreeNode.java
@@ -0,0 +1,12 @@
+// TreeNode for reference
+
+class BinaryTreeNode {
+    public int data;
+    public BinaryTreeNode left, right;
+
+    public BinaryTreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
