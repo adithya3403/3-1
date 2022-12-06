ea6abeec88d40a28e1d58b0194c228292163178e
adithya3403 <a.dty.3403@gmail.com>
2022-12-06 23:20:00 +0530

    Update 3.js

--- WT/LAB/3.js
+++ WT/LAB/3.js
@@ -1,20 +1,28 @@
-// 3. write a js program to remove duplicates from an array
+// 3) Write a Node JS program that accepts a port from the user and runs a node server at that port
 
-var readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    if (req.url == '/') { // check the URL of the current request
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        // set response content    
+        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.end();
+    }
+    else if (req.url == "/student") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.end();
+    }
+    else if (req.url == "/admin") {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.end();
+    }
+    else
+        res.end('Invalid Request!');
 });
 
-readline.question("Enter elements separated by space : ", function (line) {
-    let arr = line.split(' ').map(Number);
-    removeDupls(arr);
-    readline.close();
-});
-
-function removeDupls(arr) {
-    let set = new Set(arr);
-    console.log(set);
-}
-
-// Enter elements separated by space : 1 4 1 5 8 2 3
-// Set(6) { 1, 4, 5, 8, 2, 3 }
+server.listen(8000);
+console.log('3.js web server is running at port 8000');
