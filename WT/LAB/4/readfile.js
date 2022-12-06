24fbe6b88c218c5626f5de2e57e96e55a0c385e7
adithya3403 <a.dty.3403@gmail.com>
2022-12-06 23:20:04 +0530

    Create readfile.js

---
+++ WT/LAB/4/readfile.js
@@ -0,0 +1,9 @@
+// 4.	Write a NodeJS program to read from a file and display the content on screen – “readfile.js”
+
+var fs = require('fs');
+try {
+    var data = fs.readFileSync('file.txt', 'utf8');
+    console.log(data);
+} catch (e) {
+    console.log('Error:', e.stack);
+}
