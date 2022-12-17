caeb70b34d3988d75ada703f72a58a8bf00e1d96
adithya3403 <a.dty.3403@gmail.com>
2022-12-17 19:05:51 +0530

    Create dns.java

---
+++ CN LAB/dns.java
@@ -0,0 +1,24 @@
+// DNS implementation 
+
+import java.net.*;
+import java.util.*;
+public class dns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the domain name: ");
+        String domain = sc.nextLine();
+        try {
+            InetAddress ip = InetAddress.getByName(domain);
+            System.out.println("IP address: " + ip.getHostAddress());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        sc.close();
+    }
+}
+
+/*
+Output:
+Enter the domain name: www.google.com
+IP address: 216.58.196.164
+*/
