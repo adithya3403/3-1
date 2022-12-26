ebe338cc1dc6f6f40651cc22f2547cb5aa7bcfe1
adithya3403 <a.dty.3403@gmail.com>
2022-12-26 23:01:26 +0530

    Update 3.js

--- WT/LAB/3.js
+++ WT/LAB/3.js
@@ -1,28 +1,20 @@
 // 3) Write a Node JS program that accepts a port from the user and runs a node server at that port
 
 var http = require('http');
-
 var server = http.createServer(function (req, res) {
-    if (req.url == '/') { // check the URL of the current request
-        // set response header
+    if (req.url == '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        // set response content    
-        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.write('<html><body><p>This is home Page</p></body></html>');
         res.end();
-    }
-    else if (req.url == "/student") {
+    } else if (req.url == "/student") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.write('<html><body><p>This is student Page</p></body></html>');
         res.end();
-    }
-    else if (req.url == "/admin") {
+    } else if (req.url == "/admin") {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.write('<html><body><p>This is admin Page</p></body></html>');
         res.end();
-    }
-    else
-        res.end('Invalid Request!');
+    } else res.end('Invalid Request!');
 });
-
 server.listen(8000);
-console.log('3.js web server is running at port 8000');
+console.log('Server is running at port 8000');
