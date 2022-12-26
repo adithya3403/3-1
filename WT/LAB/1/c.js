61697011eb2ea9480d3091c639e7c38b741b8616
adithya3403 <a.dty.3403@gmail.com>
2022-12-26 23:01:12 +0530

    Update c.js

--- WT/LAB/1/c.js
+++ WT/LAB/1/c.js
@@ -1,21 +1,14 @@
 // 3. write a js program to remove duplicates from an array
 
-function removeDuplicates(num) {
-    len = num.length;
-    uniqueChars = [];
-
-    num.forEach((c) => {
-        if (!uniqueChars.includes(c)) {
-            uniqueChars.push(c);
-        }
-    });
-    return uniqueChars;
+var list = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
+var unq = [];
+for (var l = 0; l < list.length; l++) {
+    if (unq.indexOf(list[l]) == -1) {
+        unq.push(list[l]);
+    }
 }
-let Mynum = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
-result = removeDuplicates(Mynum);
-console.log("Original List:   " + Mynum);
-console.log("Unique List:    " + result);
+console.log("Original List: ", list);
+console.log("Unique List: ", unq);
 
-
-// Original List:   1,2,2,4,5,4,7,8,7,3,6
-// Unique List:    1,2,4,5,7,8,3,6
+// Original List:  [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6]
+// Unique List:  [1, 2, 4, 5, 7, 8, 3, 6]
