e2d6edf8775914ab09b97f5afd18a18ed4c742fb
adithya3403 <a.dty.3403@gmail.com>
2022-12-26 23:01:56 +0530

    Revert "Create 2nd.html"
    This reverts commit 69c496b3b4fe773342577f2d233563be5784f9d5.

--- WT/LAB/4/readfile.js
+++ WT/LAB/4/readfile.js
@@ -1,8 +1,7 @@
 // 4.	Write a NodeJS program to read from a file and display the content on screen – “readfile.js”
 
-var fs = require('fs');
 try {
-    var data = fs.readFileSync('file.txt', 'utf8');
+    var data = require('fs').readFileSync('file.txt', 'utf8');
     console.log(data);
 } catch (e) {
     console.log('Error:', e.stack);
