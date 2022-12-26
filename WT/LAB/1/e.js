875276d423b47ee94b9a6f64bcc42433c1500e3a
adithya3403 <a.dty.3403@gmail.com>
2022-12-26 23:01:15 +0530

    Update e.js

--- WT/LAB/1/e.js
+++ WT/LAB/1/e.js
@@ -1,13 +1,6 @@
 // 5. Write a JavaScript program to list the properties of a JavaScript object.
 
-let object = {
-    name: 'Jack',
-    age: 25,
-    college: 'KMIT',
-    year: 3,
-    sem: 1
-};
-let properties = Object.keys(object);
-console.log(properties);
+let list = { name: 'Jack', age: 25, college: 'KMIT', year: 3 };
+console.log(Object.keys(list));
 
-// [ 'name', 'age', 'college', 'year', 'sem' ]
+// [ 'name', 'age', 'college', 'year' ]
