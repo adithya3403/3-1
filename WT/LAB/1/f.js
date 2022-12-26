d1542adeca341315f66e352c3fcd7de70ac41834
adithya3403 <a.dty.3403@gmail.com>
2022-12-26 23:01:16 +0530

    Update f.js

--- WT/LAB/1/f.js
+++ WT/LAB/1/f.js
@@ -1,29 +1,12 @@
 // 6. Write a JavaScript function to check whether an object contains given property.
 
-// 1. hasOwnProperty() method
-let object1 = {
-    name: 'Jack',
-    age: 25,
-    college: 'KMIT',
-    year: 3,
-    sem: 1
-};
-console.log(object1.hasOwnProperty('name')); // true
+let object = { name: 'Jack', age: 25, college: 'KMIT', year: 3};
+console.log(object.hasOwnProperty('name'));
+console.log('age' in object);
+console.log(object.college);
+console.log(object.year);
 
-
-// 2. in operator Method:
-let object2 = {
-    name: 'Jack',
-    age: 25,
-    college: 'KMIT', year: 3, sem: 1
-};
-console.log('name' in object2); // true
-
-// 3. Comparing with undefined Method:
-let object3 = {
-    name: 'Jack',
-    age: 25,
-    college: 'KMIT', year: 3, sem: 1
-};
-console.log(object3.name); // Jack
-console.log(object3.fee); // undefined
+// true
+// true
+// KMIT
+// 3
