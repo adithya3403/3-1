426295cfe52fc229ddce366649197b1f436fd4e8
adithya3403 <a.dty.3403@gmail.com>
2022-12-26 23:01:58 +0530

    Update 5.js

--- WT/LAB/5/5.js
+++ WT/LAB/5/5.js
@@ -1,10 +1,6 @@
 // 5 Create a NodeJS programme that allows users to submit text and a file name, and if the file already exists, appends the text to the file. If not, make a fresh file and include the text in it.
 
-
-var fs = require('fs');
-
-// appendFile function with filename, content and callback function
-fs.appendFile('newfile.txt', `Learn Node FS module \r\n`, function (err) {
+require('fs').appendFile('newfile.txt', `Learn Node FS module \r\n`, function (err) {
     if (err) throw err;
     console.log('File is Appended successfully.');
 });
