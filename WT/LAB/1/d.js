43e2b2d4d004da3bc91a897f9f56b4e64419753d
adithya3403 <a.dty.3403@gmail.com>
2022-12-26 23:01:13 +0530

    Update d.js

--- WT/LAB/1/d.js
+++ WT/LAB/1/d.js
@@ -1,22 +1,17 @@
 // 4. write a js program to perform binary search
 
-let iterativeFunction = function (arr, x) {
-    let start = 0, end = arr.length - 1;
-    while (start <= end) {
-        let mid = Math.floor((start + end) / 2);
-        if (arr[mid] === x) return true;
-        else if (arr[mid] < x)
-            start = mid + 1;
-        else
-            end = mid - 1;
+var list = [11, 15, 23, 24, 36, 58, 62];
+var x = 62;
+var l = 0, r = list.length - 1;
+var binarySearch = function (list, x, l, r) {
+    while (l <= r) {
+        var mid = Math.floor((l + r) / 2);
+        if (list[mid] == x) return "Found at index: " + mid;
+        else if (list[mid] < x) l = mid + 1;
+        else r = mid - 1;
     }
+    return "Not Found";
+}
+console.log(binarySearch(list, x, l, r));
 
-    return false;
-};
-
-
-let arr = [1, 3, 5, 7, 8, 9];
-let x = 5;
-console.log(iterativeFunction(arr, x));
-
-// true
+// Found at index:  6
