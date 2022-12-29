0d738e7d846bf35520de8be646446c272620960f
adithya3403 <a.dty.3403@gmail.com>
2022-12-29 20:10:14 +0530

    Update dns.java

--- CN LAB/dns.java
+++ CN LAB/dns.java
@@ -11,6 +11,14 @@ public class dns {
             InetAddress ip = InetAddress.getByName(domain);
             System.out.println("IP address: " + ip.getHostAddress());
         } catch (Exception e) { System.out.println(e); }
+        System.out.print("Enter the IP address: ");
+        String ip = sc.nextLine();
+        try {
+            InetAddress domainName = InetAddress.getByName(ip);
+            System.out.println("Domain name: " + domainName.getHostName());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         sc.close();
     }
 }
@@ -19,4 +27,6 @@ public class dns {
 Output:
 Enter the domain name: www.google.com
 IP address: 216.58.196.164
+Enter the IP address: 20.207.73.82
+Domain name: maa05s18-in-f4.1e100.net
 */
